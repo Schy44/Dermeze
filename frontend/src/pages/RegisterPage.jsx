ad6e8786746cfccce43680ee2754f4b3fde754b3
safa <saimachysafa@gmail.com>
2024-12-16 03:05:02 +0600

    .

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import '../assets/Register.css'
+import '../assets/Register.css';
 
 const RegisterPage = () => {
     const navigate = useNavigate();
